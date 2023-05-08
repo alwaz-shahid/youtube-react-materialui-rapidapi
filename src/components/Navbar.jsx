81c61ea2b56dccfa3a452e6c53b379fc2605e140
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-05-08 18:39:43 +0500

    sb import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
-import { Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-import { logo } from "../utils/constants";
-import { SearchBar } from "./";
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
+// import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#00272B', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={35} />
+  <Stack
+    direction='row'
+    alignItems='center'
+    p={2}
+    sx={{
+      position: 'sticky',
+      background: '#00272B',
+      top: 0,
+      justifyContent: 'space-between',
+    }}>
+    <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt='logo' height={35} />
     </Link>
     <SearchBar />
   </Stack>
--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ export { default as ChannelDetail } from './ChannelDetail';
 export { default as Feed } from './Feed';
 export { default as Loader } from './Loader';
 export { default as Navbar } from './Navbar';
-export { default as SearchBar } from './SearchBar';
+// export { default as SearchBar } from './SearchBar';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as Videos } from './Videos';
